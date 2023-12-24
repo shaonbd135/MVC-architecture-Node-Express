0945a12d0e270620ced3a7a5703cd91b7b8327e8
shaonbd135 <shaonsimt@gmail.com>
2023-12-24 23:19:09 +0600

    Project Updated

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ const express = require("express");
 const userRouter = require("./routes/users.route");
 const productRouter = require("./routes/product.route");
 
+//CORS
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({extended: true}));
-app.use(userRouter);
-app.use(productRouter);
+app.use("/api/users",userRouter);
+app.use("/api/users",productRouter);
 
 app.use((req,res,next) => {
     res.status(404).json({
         message: "resource not found ! 404 "
     })
-})
+}) 
 
 
 app.listen(PORT , () => {
